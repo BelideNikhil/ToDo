fa4c08babfd814a52ccd6fb144f9a00b0073fb95
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2021-06-22 03:51:17 +0530

    added checkbox style on complete

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ else{
     arr.forEach(element => {
      let new_item= document.createElement("li")
      new_item.appendChild(document.createTextNode(element))
-     new_item.innerHTML +=`<a href="#"><i class="fas fa-trash-alt"></i></a>`
-     new_item.childNodes[1].setAttribute("class","list_item")
+     new_item.innerHTML +=`<input type="checkbox"><a href="#"><i class="fas fa-trash-alt"></i></a>`
+     new_item.childNodes[2].setAttribute("class","list_item")
      full_list.appendChild(new_item)
     });
  }
@@ -32,21 +32,18 @@ add_task.addEventListener("click",()=>{
     else{
         let new_item= document.createElement("li")
         new_item.appendChild(document.createTextNode(`${user_input.value}`))
-        new_item.innerHTML +=`<a href="#"><i class="fas fa-trash-alt"></i></a>`
-        new_item.childNodes[1].setAttribute("class","list_item")
+        new_item.innerHTML +=`<input type="checkbox"><a href="#"><i class="fas fa-trash-alt"></i></a>`
+        new_item.childNodes[2].setAttribute("class","list_item")
         full_list.appendChild(new_item)
         arr.push(user_input.value)
         localStorage.setItem("tasks",JSON.stringify(arr))
     }
 })
 
-
 // removing task from list
 document.body.addEventListener("click",(e)=>{
     if(e.target.parentElement.classList.contains("list_item")){
-        console.log(e.target.parentElement.parentElement.innerText)
         full_list.removeChild(e.target.parentElement.parentElement)
-        
         // removing element from array after knowing it's index
         let idx = arr.indexOf(e.target.parentElement.parentElement.innerText);
         if(idx !=-1){
@@ -55,6 +52,37 @@ document.body.addEventListener("click",(e)=>{
         localStorage.setItem("tasks",JSON.stringify(arr))
     }
 })
+
+
+// crossing the li and removing from local if the task is complete
+document.body.addEventListener("click",(e)=>{
+    if(e.target.checked){
+        e.target.parentElement.setAttribute("class","checkbox")
+    }
+
+    if(e.target.parentElement.classList.contains("checkbox")){
+        console.log(arr)
+        let a= confirm("did you complete it?")
+        if(a){
+            let idx = arr.indexOf(e.target.parentElement.innerText);
+            if(idx !=-1){
+                arr.splice(idx,1)
+            }
+            localStorage.setItem("tasks",JSON.stringify(arr))
+            setTimeout(()=>{
+                window.location.reload()
+            },1500)       
+        }
+        else{
+            window.location.reload()
+        }   
+    }
+})
+
+
+
+
+
 
 
 
