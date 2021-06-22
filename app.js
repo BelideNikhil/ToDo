d5e17c898a7d870b4700f3669ddeb7cb8aa777ce
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2021-06-23 01:15:52 +0530

    add strike on complete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let user_input=document.querySelector("#user-input")
 const add_task=document.querySelector("#add")
 let full_list=document.querySelector(".todo-list")
 let list_item=document.querySelector(".list_item")
+let checkbox=document.querySelector("input[type=\"checkbox\"]")
 
 
 // getting tasks fro local storage if any exist else set it 
@@ -21,7 +22,28 @@ else{
      new_item.childNodes[2].setAttribute("class","list_item")
      full_list.appendChild(new_item)
     });
- }
+}
+
+
+// 
+if(localStorage.getItem("completed")==="undefined"){
+    let arr2=[]
+}
+else if(localStorage.getItem("completed")===null){
+    let arr2=[]
+    localStorage.setItem("completed",JSON.stringify(arr2))
+}
+else{
+    arr2=JSON.parse(localStorage.getItem("completed"))
+    console.log(arr2)
+    arr2.forEach(element => {
+        let new_item= document.createElement("li")
+        new_item.appendChild(document.createTextNode(element))
+        new_item.innerHTML +=`<a href="#"><i class="fas fa-trash-alt"></i></a>`
+        new_item.setAttribute("class","strike ")
+        full_list.appendChild(new_item)
+    });
+}
 
 
 // adding task to a list
@@ -33,51 +55,83 @@ add_task.addEventListener("click",()=>{
         let new_item= document.createElement("li")
         new_item.appendChild(document.createTextNode(`${user_input.value}`))
         new_item.innerHTML +=`<input type="checkbox"><a href="#"><i class="fas fa-trash-alt"></i></a>`
-        new_item.childNodes[2].setAttribute("class","list_item")
+        new_item.childNodes[2].setAttribute("class","list_item ")
         full_list.appendChild(new_item)
         arr.push(user_input.value)
         localStorage.setItem("tasks",JSON.stringify(arr))
+        window.location.reload()
     }
 })
 
-// removing task from list
+function run(){
+    
+for( let i=0;i<arr2.length;i++){
+    for(let j=0;j<arr.length;j++){
+        if(arr2[i]===arr[j]){
+            console.log(arr2[i])
+            console.log( arr.splice(arr2[i]))
+        }
+    }
+}
+}
+
+
+// removing task from completed list
 document.body.addEventListener("click",(e)=>{
+    if(e.target.parentElement.parentElement.classList.contains("strike")){
+    
+        full_list.removeChild(e.target.parentElement.parentElement)
+        // removing element from array after knowing it's index
+        let idx = arr2.indexOf(e.target.parentElement.parentElement.innerText);
+        if(idx !=-1){
+            arr2.splice(idx,1)
+        }
+        
+        localStorage.setItem("tasks",JSON.stringify(arr))
+        localStorage.setItem("completed",JSON.stringify(arr2))
+        
+    }
+})
+
+// deleting task from list
+document.body.addEventListener("click",(e)=>{
+    
     if(e.target.parentElement.classList.contains("list_item")){
+        run
         full_list.removeChild(e.target.parentElement.parentElement)
         // removing element from array after knowing it's index
         let idx = arr.indexOf(e.target.parentElement.parentElement.innerText);
         if(idx !=-1){
             arr.splice(idx,1)
         }
+        
         localStorage.setItem("tasks",JSON.stringify(arr))
+        localStorage.setItem("completed",JSON.stringify(arr2))
     }
 })
+
 
 
 // crossing the li and removing from local if the task is complete
 document.body.addEventListener("click",(e)=>{
     if(e.target.checked){
-        e.target.parentElement.setAttribute("class","checkbox")
-    }
-
-    if(e.target.parentElement.classList.contains("checkbox")){
-        console.log(arr)
-        let a= confirm("did you complete it?")
-        if(a){
-            let idx = arr.indexOf(e.target.parentElement.innerText);
-            if(idx !=-1){
-                arr.splice(idx,1)
-            }
-            localStorage.setItem("tasks",JSON.stringify(arr))
-            setTimeout(()=>{
-                window.location.reload()
-            },1500)       
+        arr2.push(e.target.parentElement.innerText)
+        let idx = arr.indexOf(e.target.parentElement.innerText);
+        console.log(idx)
+        if(idx !=-1){
+            arr.splice(idx,1)
         }
-        else{
-            window.location.reload()
-        }   
+        localStorage.setItem("tasks",JSON.stringify(arr))
+        localStorage.setItem("completed",JSON.stringify(arr2))
+        window.location.reload()
     }
+    
+    
 })
+
+
+
+
 
 
 
