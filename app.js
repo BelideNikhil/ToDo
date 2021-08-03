2ec9d25538c13521d14b8a5f59a1c8924337212f
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2021-08-03 22:59:23 +0530

    refactor strike,fix reload

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let user_input=document.querySelector("#user-input")
 const add_task=document.querySelector("#add")
-let full_list=document.querySelector(".todo-list")
+let todo_list=document.querySelector(".todo-list") 
 let list_item=document.querySelector(".list_item")
-let checkbox=document.querySelector("input[type=\"checkbox\"]")
+let checkbox=document.querySelector("input[type='checkbox']")
 
 let no_data=document.querySelector("#error")
 
@@ -21,7 +21,7 @@ else{
      new_item.appendChild(document.createTextNode(element))
      new_item.innerHTML +=`<input type="checkbox"><a href="#"><i class="far fa-trash-alt"></i></a>`
      new_item.childNodes[2].setAttribute("class","list_item")
-     full_list.appendChild(new_item)
+     todo_list.appendChild(new_item)
     });
 }
 
@@ -41,16 +41,17 @@ else{
         new_item.appendChild(document.createTextNode(element))
         new_item.innerHTML +=`<a href="#"><i class="far fa-trash-alt"></i></a>`
         new_item.setAttribute("class","strike ")
-        full_list.appendChild(new_item)
+        todo_list.appendChild(new_item)
     });
 }
 
 // adding task to a list
 add_task.addEventListener("click",()=>{
     if(user_input.value===""){
+        no_data.style.display='block'
         no_data.innerHTML="Please enter data to add a task."
         setTimeout(()=>{
-            no_data.innerHTML=""
+            no_data.style.display='none'
         },2000)
     }
     else{
@@ -58,55 +59,35 @@ add_task.addEventListener("click",()=>{
         new_item.appendChild(document.createTextNode(`${user_input.value}`))
         new_item.innerHTML +=`<input type="checkbox"><a href="#"><i class="far fa-trash-alt"></i></a>`
         new_item.childNodes[2].setAttribute("class","list_item ")
-        full_list.appendChild(new_item)
+        todo_list.appendChild(new_item)
         arr.push(user_input.value)
         localStorage.setItem("tasks",JSON.stringify(arr))
-        setTimeout(window.location.reload(),0.1)
-    }
-})
-
-// this pops from main arr and psuhes to completed array
-function run(){
-    for( let i=0;i<arr2.length;i++){
-        for(let j=0;j<arr.length;j++){
-            if(arr2[i]===arr[j]){
-                arr2[i]
-                arr.splice(arr2[i])
-            }
-        }
-    }
-}
-
-// removing task from completed list
-document.body.addEventListener("click",(e)=>{
-    if(e.target.parentElement.parentElement.classList.contains("strike")){
-        full_list.removeChild(e.target.parentElement.parentElement)
-        // removing element from array after knowing it's index
-        let idx = arr2.indexOf(e.target.parentElement.parentElement.innerText);
-        if(idx !=-1){
-            arr2.splice(idx,1)
-        }
-        localStorage.setItem("tasks",JSON.stringify(arr))
-        localStorage.setItem("completed",JSON.stringify(arr2))
     }
 })
 
 // deleting task from list
 document.body.addEventListener("click",(e)=>{
     if(e.target.parentElement.classList.contains("list_item")){
-        run
-        full_list.removeChild(e.target.parentElement.parentElement)
+        todo_list.removeChild(e.target.parentElement.parentElement)
         // removing element from array after knowing it's index
         let idx = arr.indexOf(e.target.parentElement.parentElement.innerText);
         if(idx !=-1){
             arr.splice(idx,1)
         }
-        localStorage.setItem("tasks",JSON.stringify(arr))
-        localStorage.setItem("completed",JSON.stringify(arr2))
+        setter()
+    }
+    if(e.target.parentElement.parentElement.classList.contains("strike")){
+        todo_list.removeChild(e.target.parentElement.parentElement)
+        // removing element from array after knowing it's index
+        let idx = arr2.indexOf(e.target.parentElement.parentElement.innerText);
+        if(idx !=-1){
+            arr2.splice(idx,1)
+        }
+        setter()
     }
 })
 
-// crossing the li and removing from local if the task is complete
+// crossing the li and moving to another array
 document.body.addEventListener("click",(e)=>{
     if(e.target.checked){
         arr2.push(e.target.parentElement.innerText)
@@ -115,16 +96,15 @@ document.body.addEventListener("click",(e)=>{
         if(idx !=-1){
             arr.splice(idx,1)
         }
-        localStorage.setItem("tasks",JSON.stringify(arr))
-        localStorage.setItem("completed",JSON.stringify(arr2))
-        setTimeout(window.location.reload(),0.1)
+        setter()
+        setTimeout(window.location.reload(),1)
     }
 })
 
-
-
-
-
+function setter(){
+    localStorage.setItem("tasks",JSON.stringify(arr))
+    localStorage.setItem("completed",JSON.stringify(arr2))
+}
 
 
 
